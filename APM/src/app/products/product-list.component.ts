e4f05b7ed83f55e23329b8946a1ae2505cf1dc71
EzharAnsari <37619206+EzharAnsari@users.noreply.github.com>
2024-07-17 20:16:30 +0530

    We make reactive streams using observable

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,36 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { catchError, EMPTY, Observable, of } from 'rxjs';
+import { catchError, EMPTY } from 'rxjs';
 import { ProductCategory } from '../product-categories/product-category';
 
-import { Product } from './product';
 import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories: ProductCategory[] = [];
 
-  products: Product[] = [];
-  products$!: Observable<Product[]> ;
+  products$ = this.productService.products$
+  .pipe(
+    catchError( err => {
+      this.errorMessage = err;
+      // return of([]);
+      return EMPTY;
+    })
+  );
 
 
   constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.products$ = this.productService.getProducts()
-      .pipe(
-        catchError( err => {
-          this.errorMessage = err;
-          // return of([]);
-          return EMPTY;
-        })
-      );
-  }
 
   onAdd(): void {
     console.log('Not yet implemented');
