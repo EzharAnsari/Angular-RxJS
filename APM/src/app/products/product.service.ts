a67448abef3af2f7b137d9edfa1b20f75a4b3307
EzharAnsari <37619206+EzharAnsari@users.noreply.github.com>
2024-07-17 21:29:00 +0530

    We combined two streams

--- APM/src/app/products/product.service.ts
+++ APM/src/app/products/product.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { catchError, map, Observable, tap, throwError } from 'rxjs';
+import { catchError, combineLatest, map, Observable, tap, throwError } from 'rxjs';
 
 import { Product } from './product';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,17 +15,25 @@ export class ProductService {
 
   products$ = this.http.get<Product[]>(this.productsUrl)
   .pipe(
-    map(products =>
-        products.map(product => ({
-            ... product,
-            price: product.price ? product.price * 1.5 : 0 
-        }))
-      ),
     tap(data => console.log('Products: ', JSON.stringify(data))),
     catchError(this.handleError)
   );
+
+  productWithCategory$ = combineLatest([
+    this.products$,
+    this.productCategorySerice.productCategories$
+  ]).pipe(
+    map(([ products, categories ]) => 
+      products.map(product => ({
+        ... product,
+        price: product.price ? product.price * 1.5 : 0,
+        category: categories.find(c => c.id === product.categoryId)?.name,
+        searchKey: [product.productName]
+      }) as Product)
+    )
+  );
   
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private productCategorySerice: ProductCategoryService) { }
 
   private fakeProduct(): Product {
     return {
